8c42b70d3d8a99b624f724798164413729e46284
zpxio <7874624+zpxio@users.noreply.github.com>
2022-11-13 08:41:30 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Go Report Card](https://goreportcard.com/badge/zpxio/recipe-web)](https://goreportcard.com/report/zpxio/recipe-web) ![Build](https://github.com/zpxio/recipe-web/actions/workflows/build.yml/badge.svg) ![Tests](https://github.com/zpxio/recipe-web/actions/workflows/tests.yml/badge.svg) [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://github.com/zpxio/recipe-web/blob/master/LICENSE)
+
+# recipe-web
---
+++ cmd/recipeserv/main.go
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2022 zpxio (Jeff Sharpe)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+)
+
+func main() {
+	// Setup
+	e := echo.New()
+	e.Logger.SetLevel(log.INFO)
+	e.GET("/", func(c echo.Context) error {
+		time.Sleep(5 * time.Second)
+		return c.JSON(http.StatusOK, "OK")
+	})
+
+	// Start server
+	go func() {
+		if err := e.Start(":1323"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server with a timeout of 10 seconds.
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	// Execute a timed shutdown of the server
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+}
